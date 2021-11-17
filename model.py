b354d1b26b9df6f3a2c68a2855c9d35c22c60dee
Dnya <donyashunbash1@gmail.com>
2021-11-17 12:27:54 +0330

    model skeletion

---
+++ model.py
@@ -0,0 +1,18 @@
+import tkinter as tk 
+
+flower_List = [ "Rose" , "Lily" , "Lotus" , "Daisy" , "Jasmin" , "Sunflower" , "Marigold" , "Orchid" , "Blueble" , "Dahlia" ]
+
+exist_Flower = [ "Rose" , "Sunflower" , "Daisy" , "Lotus" , "Marigold" ]
+
+
+
+class Edit():
+    pass #Ayda
+
+
+class Add():
+    pass #Donya
+
+
+
+
