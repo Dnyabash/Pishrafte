660288e5ed793816ee325c1e210051d0817f49bf
Dnya <donyashunbash1@gmail.com>
2021-11-18 12:21:46 +0330

    Menu items

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ flower_List = [ "Rose" , "Lily" , "Lotus" , "Daisy" , "Jasmin" , "Sunflower" , "
 
 exist_Flower = [ "Rose" , "Sunflower" , "Daisy" , "Lotus" , "Marigold" ]
 
-
+new = []
 
 class Edit():
     pass #Ayda
@@ -16,22 +16,30 @@ class Add( tk.Frame ):
         self.grid( row = 0 , column = 0 , padx = 200  , pady = 100  )
         self.config ( bg = "#B0E0E6" )
         
-        self.fr_menu = tk.Frame( self , bd = 1 , relief = "solid" , padx = 100 , bg = "#c0c0c0" )
-        self.fr.grid ( row = 0 , column = 0 )
-        self.lbl_meno = tk.Label( self.fr_menu , text = "Meno" , fg = "#483D8B" , bg = "#c0c0c0" )
-        self.lbl_meno.grid ( row = 0 , column = 0  , sticky = "w" )
+        self.fr_menu = tk.Frame( self , bd = 1 , relief = "solid" , padx = 100 , bg = "#c0c0c0" , width = 300 , height = 500 )
+        self.fr_menu.grid ( row = 0 , column = 0 )
+        self.lbl_menu = tk.Label( self.fr_menu , text = "Menu" , fg = "#483D8B" , bg = "#c0c0c0" )
+        self.lbl_menu.grid ( row = 0 , column = 0  , sticky = "w" )
+    
+        i = 0 
+        for flower in flower_List:
+            
+            if flower in exist_Flower:
+                pass
+            else :
+                
+                self.lbl_flower = tk.Label( self.fr_menu , text = flower , fg = "#483D8B" , bg = "#c0c0c0" )
+                self.lbl_flower.grid( row = i , column = 0 , pady = 20 , sticky = "w" )
+                new.append( flower )
 
-
-
-
-
+            i = i + 1
 
 
 
 
 
 win = tk.Tk() 
-win.geometry ("1000x800+300+150")
+win.geometry ("1000x700+300+50")
 win.title( " Flower shopping " )
 win.config( bg ="#FFEFD5")
 flower1 = Add( win )
